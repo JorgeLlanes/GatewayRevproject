e6622eb106d4298908d504c698d071d39c330414
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-04-13 21:44:37 -0500

    Added JavaScript File and new mods

---
+++ index.js
@@ -0,0 +1,7 @@
+const open = prompt("R2H Instructure please ENTER your NAME:");
+const open2 = prompt("THANK YOU FOR EVERYTHING! Now check the console PLS!");
+
+if (open); {
+
+    console.log("You are the BEST!")
+}
