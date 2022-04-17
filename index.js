b88741666d81023f77c7546799203766b52f306d
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-04-17 18:20:57 -0500

    New JS mods

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const open = prompt("R2H Instructure please ENTER your NAME:");
-const open2 = prompt("THANK YOU FOR EVERYTHING! Now check the console PLS!");
 
-if (open); {
+if (open) {
+    alert("YOU ARE THE BEST!!! THANK FOR EVERYTHING!!!")
+};
 
-    console.log("You are the BEST!")
-}
+
